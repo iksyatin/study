5dc6e54bd1d25bcac9a66845d11c827708f12f03
Sergey <iksyatin@gmail.com>
2020-10-19 00:46:30 +0300

    first commit

---
+++ JS/scripts.js
@@ -0,0 +1,100 @@
+'use strict';
+// // --------------------------------------------
+
+
+
+
+
+
+// // --------------------------------------------
+//
+// console.log('arr' + '-object');
+// console.log(4 + +'5');
+//
+// let incr = 10,
+//     decr = 10;
+// incr++;
+// decr--;
+// console.log(incr);
+// console.log(decr);
+//
+// ++incr;
+// --decr;
+// console.log(incr);
+// console.log(decr);
+//
+// console.log(5%2);
+//
+// console.log((2+2)*2 === +'8');
+// console.log((2+2)*2 !== '8');
+// console.log((2+2)*2 !== +'8');
+//
+//
+// // && это И
+// // || это или
+//
+// const isChecked = true,
+//     isClose = false;
+// console.log(isChecked || isClose);
+// console.log(isChecked && !isClose);
+//
+
+// --------------------------------------
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+//
+// const user = 'Ivan';
+// alert(`Привет, ${user} сраный`);
+
+// -------------------------------
+
+
+// alert('Hello');
+//
+// // const result = confirm('Are you here?');
+// // console.log(result);
+//
+// // const answer = +prompt('Вам есть 18?','18');
+// // console.log((answer + 10));
+//
+// const answers = [];
+// answers[0] = prompt('Как ваше имя?', '')
+// answers[1] = prompt('Как ваше фамилия?', '')
+// answers[2] = prompt('Сколько вам лет?', '')
+//
+// // document.write(answers);
+// console.log(typeof(answers));
+// console.log(typeof(null));
+// // ----------------------------------------
+
+
+
+
+// let number = 4.6;
+//
+// console.log(-4/0);
+// console.log('string' * 9);
+//
+// const persone = 'Alex';
+// console.log(persone);
+//
+// const bool = true;
+//
+// // console.log(something);
+//
+// let und;
+// console.log(und);
+//
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+//
+// // console.log(obj.name);
+// console.log(obj ['name']);
+//
+// let arr = ['plim.png', 'orange.jpeg', 6, {}, [], 'apple.bmp'];
+// console.log(arr[1]);
+
